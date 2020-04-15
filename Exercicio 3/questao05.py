22915cba1ed19eefb39b461fe3ee5b6664a3b73b
WeversonCelio <weversoncelio25@gmail.com>
2020-04-15 00:12:42 -0300

    exercicios 3: questao 5

---
+++ Exercicio 3/questao05.py
@@ -0,0 +1,9 @@
+#Leitura do Teclado
+nome = input('Digite seu nome: ')
+print ('Ola ', nome)
+cidade = input('Digite sua cidade: ')
+print('Senhor ', nome, ' da cidade de ', cidade , 'bem vindo!')
+frase = input('Digite sua frase: ')
+print('Sua frase e = ', frase[::-1])
+
+
