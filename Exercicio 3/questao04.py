ffd8d43c3d0f8e11f46e9c86a3eaa85adda41ea3
WeversonCelio <weversoncelio25@gmail.com>
2020-04-14 23:57:03 -0300

    exercicios 3: questao 4

---
+++ Exercicio 3/questao04.py
@@ -0,0 +1,7 @@
+#Dada a frase Python é muito legal., use fatiamento para dar nome
+#às variáveis contendo cada palavra. O resultado final deve ser:
+frase = 'Python é muito legal'
+print(frase[0:6])
+print(frase[7])
+print(frase[9:14])
+print(frase[15:21])
