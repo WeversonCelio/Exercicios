5c1d6ecb757bf844d074ab20d2834863a5c7c6e6
WeversonCelio <weversoncelio25@gmail.com>
2020-04-14 22:40:27 -0300

    exercicios 3: questao 1

--- Exercicio 3/questao01.py
+++ Exercicio 3/questao01.py
@@ -1,1 +1,10 @@
-## questao 01
+## Supondo que a cotação do dólar esteja em R$ 3,25, salve esse
+## valor em uma variável e utilize-o para calcular quanto você teria
+## ao cambiar R$ 65,00 para dólares.
+
+dolar = 3.25
+reais = 65
+
+resultado = 65/ 3.25
+
+print('voce podera compra', resultado, ' Dolares')
